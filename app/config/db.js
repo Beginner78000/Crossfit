5b58794ec916e5b624475e17fa62b2224dc5f250
Marie HART <50957448+Beginner78000@users.noreply.github.com>
2022-03-18 20:57:45 +0100

    modif deployement Heroku

--- app/config/db.js
+++ app/config/db.js
@@ -5,6 +5,16 @@
 const debug = require('debug')('SQL:log');
 const { Pool } = require('pg');
 
+const config = {
+    connectionString: process.env.DATABASE_URL
+};
+
+if (process.env.NODE_ENV === 'production') {
+    config.ssl = {
+        rejectUnauthorized: false,
+    };
+}
+
 const pool = new Pool();
 
 module.exports = {
